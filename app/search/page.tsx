ae307e3b4219e96420d2a78d83f8d695137ae21f
Marcelo Tosco <capy.net@gmail.com>
2025-02-27 17:30:37 +0100

    Added embedding for vectorial records.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
-import {prompt} from "@/services/ia";
+import {embed, prompt} from "@/services/ia";
 
 export default async function Search() {
     const answer = prompt("De que color es el cielo? dame una respuesta corta.")
+    //const answer = embed("escribe la palabra hola")
     return <p>{answer}</p>;
 }
--- services/ia.ts
+++ services/ia.ts
@@ -1,8 +1,15 @@
 const {GoogleGenerativeAI} = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export async function prompt(prompt: string): Promise<string> {
-    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({model: process.env.GEMINI_MODEL});
     const result = await model.generateContent(prompt);
     return result.response.text()
+}
+
+
+export async function embed(prompt: string): Promise<number[]> {
+    const model = genAI.getGenerativeModel({model: "text-embedding-004"});
+    const result = await model.embedContent(prompt);
+    return result.embedding.values
 }
