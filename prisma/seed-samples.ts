858f666bd46837db9d69c415349f0e459be13b6e
Marcelo Tosco <capy.net@gmail.com>
2025-03-20 20:49:31 +0100

    Enhanced npm db related commands. Updated README.md

--- prisma/seed-samples.ts
+++ prisma/seed-samples.ts
@@ -1,14 +1,11 @@
 // prisma/seed.ts
 import {PrismaClient} from '@prisma/client'
-import seedLanguages from './seeders/languages'
-import seedPaymentMethods from './seeders/paymentMethods'
+import seedProfiles from './seeders/profiles'
 
 const prisma = new PrismaClient()
 
 async function main() {
-    // Run seeders in order
-    await seedLanguages(prisma)
-    await seedPaymentMethods(prisma)
+    await seedProfiles(prisma)
 }
 
 main()
