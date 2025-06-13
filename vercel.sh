58dad25ff3a17056bb6bc6d509263c3771b29a0e
Marcelo Tosco <capy.net@gmail.com>
2025-06-13 14:21:46 +0200

    Temporary enabled development tasks on prod for testing purposes.

--- vercel.sh
+++ vercel.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [[ $VERCEL_ENV == "production"  ]] ; then
-  npm run build
+  #  npm run build
+  # Temporary enabled development tasks on prod for testing purposes.
+  npm run build:development
 else
   npm run build:development
 fi
