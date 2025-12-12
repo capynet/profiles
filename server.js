a772f738485fcd440fedb6550d91f6564ead504c
Marcelo Tosco <capy.net@gmail.com>
2025-12-12 19:13:01 +0100

    Updated versions.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const { createServer } = require('https')
-const { parse } = require('url')
-const next = require('next')
-const fs = require('fs')
-const path = require('path')
+import { createServer } from 'https'
+import { parse } from 'url'
+import next from 'next'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
